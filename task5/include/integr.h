04ca1db39628c15331e77f244814b0a0700ab4d7
kseniyagimgina <kgimgina07@mail.ru>
2026-04-05 22:17:14 +0600

    Задание 5. Вычисление определенного интеграла

---
+++ task5/include/integr.h
@@ -0,0 +1,6 @@
+#ifndef INTEGR_H
+#define INTEGR_H
+
+double integral(double (*f)(double), double a, double b, unsigned int n);
+
+#endif
---
+++ task5/src/test5.c
@@ -0,0 +1,14 @@
+#include "integr.h"
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+double x1(double x) { return x; }
+double x2(double x) { return x * x; }
+
+void test_integral(double (*f)(double),
+                   double a, double b, int n, double expected, double eps) {
+    double result = integral(f, a, b, n);
+    printf("  Результат: %.10f\n", result);
+    assert(fabs(result - expected) < eps);
+}
