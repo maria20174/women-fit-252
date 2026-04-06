e0cfd10a378324f422cf4eb4b61e59b02a9e79eb
kseniyagimgina <kgimgina07@mail.ru>
2026-04-06 19:59:39 +0600

    task5

---
+++ task5/src/integr.c
@@ -0,0 +1,15 @@
+#include "integr.h"
+#include <math.h>
+
+double integral(double (*f)(double), double a, double b, unsigned int n) {
+    if (n == 0) return 0.0;
+    
+    double h = (b - a) / n;
+    double sum = 0.5 * (f(a) + f(b));
+    
+    for (unsigned int i = 1; i < n; i++) {
+        sum += f(a + i * h);
+    }
+    
+    return sum * h;
+}
