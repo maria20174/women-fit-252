539494dc0c65453fa99a0e28a310224b3a8c7e18
kseniyagimgina <kgimgina07@mail.ru>
2026-05-29 17:30:40 +0600

    fix format

--- task7/include/stack.h
+++ task7/include/stack.h
@@ -4,20 +4,19 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-#define SIZE 100 
+#define SIZE 100
 
 typedef struct {
-    int data[SIZE]; 
-    size_t t;            
+  int data[SIZE];
+  size_t t;
 } Stack;
 
-void stack_init(Stack* s);
+void stack_init(Stack *s);
 
-bool stack_empty(const Stack* s);
+bool stack_empty(const Stack *s);
 
-bool stack_append(Stack* s, int a);
+bool stack_append(Stack *s, int a);
 
-bool stack_delete(Stack* s, int* out);
-
+bool stack_delete(Stack *s, int *out);
 
 #endif
